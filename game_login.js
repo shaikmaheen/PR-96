bf45a98fc57084cd32f595df272fa4c687318861
shaikmaheen <136730155+shaikmaheen@users.noreply.github.com>
2023-10-02 19:58:13 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,11 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+    //Set "player1_name" using localStorage.setItem() function and assign it to player1_name
+	  //Set "player2_name" using localStorage.setItem() function and assign it to player2_name
+
+    window.location = "game_page.html";
+}
+
